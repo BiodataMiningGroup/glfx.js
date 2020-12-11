a5721882d7e6689df8ee2702b50c5825da5713d1
Robin van Kevelaer <rvankevelaer@techfak.uni-bielefeld.de>
2020-12-11 14:57:27 +0100

    Fix adjustgamma shader, remove test index.html

---
+++ src/filters/adjust/adjustgamma.js
@@ -0,0 +1,23 @@
+/**
+ * @filter           Gamma Adjustment
+ * @description      Gamma correction using a given gamma
+ * @param gamma      Gamma exponent between 0 and 5
+ */
+function adjustGamma(gamma) {
+    gl.adjustGamma = gl.adjustGamma || new Shader(null, '\
+        uniform sampler2D texture;\
+        uniform float gamma;\
+        varying vec2 texCoord;\
+        void main() {\
+            vec4 color = texture2D(texture, texCoord);\
+            color.rgb = vec3(pow(color.r, gamma), pow(color.g, gamma), pow(color.b, gamma));\
+            gl_FragColor = color;\
+        }\
+    ');
+
+    simpleShader.call(this, gl.adjustGamma, {
+        gamma: clamp(0.0, gamma, 5.0)
+    });
+
+    return this;
+}
